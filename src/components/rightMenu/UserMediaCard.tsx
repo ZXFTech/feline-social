a028e656ff1cc201f0197247129262f851cc6489
Feline <zxf_tech@outlook.com>
2025-05-06 21:28:33 +0800

    添加用户近期分享组件

--- src/components/rightMenu/UserMediaCard.tsx
+++ src/components/rightMenu/UserMediaCard.tsx
@@ -1,8 +1,21 @@
+import prisma from "@/lib/client";
 import { User } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-const UserMediaCard = ({ user }: { user: User }) => {
+const UserMediaCard = async ({ user }: { user: User }) => {
+  const postWithMedia = await prisma.post.findMany({
+    where: {
+      userId: user.id,
+      img: {
+        not: null,
+      },
+    },
+    take: 8,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
       {/* TOP */}
@@ -12,72 +25,20 @@ const UserMediaCard = ({ user }: { user: User }) => {
           See all
         </Link>
       </div>
-      <div className="flex gap-4 justify-between flex-wrap">
-        <div className="relative w-1/5 h-20">
-          <Image
-            src="https://images.pexels.com/photos/31666543/pexels-photo-31666543.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-20">
-          <Image
-            src="https://images.pexels.com/photos/31666543/pexels-photo-31666543.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-20">
-          <Image
-            src="https://images.pexels.com/photos/31666543/pexels-photo-31666543.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-20">
-          <Image
-            src="https://images.pexels.com/photos/31666543/pexels-photo-31666543.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-20">
-          <Image
-            src="https://images.pexels.com/photos/31666543/pexels-photo-31666543.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-20">
-          <Image
-            src="https://images.pexels.com/photos/31666543/pexels-photo-31666543.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-20">
-          <Image
-            src="https://images.pexels.com/photos/31666543/pexels-photo-31666543.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="relative w-1/5 h-20">
-          <Image
-            src="https://images.pexels.com/photos/31666543/pexels-photo-31666543.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            fill
-            className="object-cover rounded-md"
-          />
-        </div>
-      </div>
+      {postWithMedia.length
+        ? postWithMedia.map((item) => (
+            <div key={item.id} className="flex gap-4 justify-between flex-wrap">
+              <div className="relative w-1/5 h-20">
+                <Image
+                  src={item.img!}
+                  alt=""
+                  fill
+                  className="object-cover rounded-md"
+                />
+              </div>
+            </div>
+          ))
+        : "No media found!"}
     </div>
   );
 };
